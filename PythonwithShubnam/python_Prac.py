f21bdea54dbfd7bd04acf75e115db17957a0d4d2
muhammad muzammil <yoursmuzammil16070@gmail.com>
2025-02-26 17:11:20 +0500

    first commit

---
+++ PythonwithShubnam/python_Prac.py
@@ -0,0 +1,6 @@
+number1 = input("Enter your first number: ")
+number2 = input("Enter your second number: ")
+
+sum_of_numbers = int(number1) + int(number2)  # Convert input to integers and add them
+
+print("The sum of two numbers is:", sum_of_numbers)  # Print the result
