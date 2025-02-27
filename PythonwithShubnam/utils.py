f32e6c652a1a2b48bcf04f2ea276a59f0f80a419
muhammad muzammil <yoursmuzammil16070@gmail.com>
2025-02-27 14:59:13 +0500

    first commit

---
+++ PythonwithShubnam/utils.py
@@ -0,0 +1,8 @@
+# Operating system level stuff
+
+import os
+
+# print(os.system('wmic logicaldisk get size,freespace,caption'))
+# print(os.system('net stats workstation'))
+
+print(os.system('sysinfo'))
