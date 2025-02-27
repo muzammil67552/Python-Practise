676f31748a945765704963d4d82939d6ab3e9287
muhammad muzammil <yoursmuzammil16070@gmail.com>
2025-02-27 14:35:28 +0500

    first commit

---
+++ PythonwithShubnam/loop.py
@@ -0,0 +1,16 @@
+list_of_name = ["google", "microsoft","apple","facebook","azamon aws","netflix"]
+print(list_of_name)
+list_of_name.append("twitter")
+print(list_of_name)
+list_of_name.insert(2, "instagram")
+print(list_of_name)
+
+# Iteration of Array List
+for name in list_of_name:
+    # there is tree dots are called identeration
+    print(name)
+
+# if you write something in 100 times then write 
+
+for items in range (0, 100):
+ print("Hello Worlds")
